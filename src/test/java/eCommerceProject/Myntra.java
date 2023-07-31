b06034754d117124309993875635e6fb5e102122
ksandeep26 <sandeepkushawaha87@gmail.com>
2023-07-31 17:29:27 +0530

    [Sandeep] Committing my 1st code.

---
+++ src/test/java/eCommerceProject/Myntra.java
@@ -0,0 +1,17 @@
+package eCommerceProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Myntra {
+  @Test
+  public void myntraTest() throws InterruptedException{
+	  WebDriver driver = new ChromeDriver();
+	  Reporter.log("opening myntra", true);
+	  driver.get("https://www.myntra.com/");
+	  Thread.sleep(2000);
+	  driver.close();
+  }
+}
