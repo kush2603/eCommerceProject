4a981beffe02e95928a8d2bd1671281b1743431a
kush2603 <kushawaha.sandeep@gmail.com>
2023-08-19 12:04:53 +0530

    [Added one more method]

--- src/test/java/eCommerceProject/MeeShow.java
+++ src/test/java/eCommerceProject/MeeShow.java
@@ -1,5 +1,8 @@
 package eCommerceProject;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
@@ -10,6 +13,7 @@ public class MeeShow {
   public void meeShowTest() throws InterruptedException {
 	  WebDriver driver = new ChromeDriver();
 	  driver.get("https://www.meeshow.com");
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	  Reporter.log("opening meeshow", true);
 	  Thread.sleep(2000);
 	  driver.manage().window().maximize();
