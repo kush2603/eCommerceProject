01d63f1515277a8d3e6c6b67d005356ef964f637
kush2603 <kushawaha.sandeep@gmail.com>
2023-10-02 11:58:05 +0530

    [added one driver launching code]

--- src/test/java/eCommerceProject/Amazon.java
+++ src/test/java/eCommerceProject/Amazon.java
@@ -13,6 +13,7 @@ public class Amazon {
 	  Reporter.log("opening amazon", true);
 	  driver.get("https://www.amazon.in/");
 	  Thread.sleep(2000);
+	  driver.manage().window().minimize();
 	  driver.close();
 	  
   }
